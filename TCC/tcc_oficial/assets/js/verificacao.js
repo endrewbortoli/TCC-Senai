5a7964d143191f98966720dd1c13be9288c22fff
EndrewBortoli <endrew.ryan977@gmail.com>
2024-09-20 08:17:46 -0300

    A

--- TCC/tcc_oficial/assets/js/verificacao.js
+++ TCC/tcc_oficial/assets/js/verificacao.js
@@ -1,25 +1,25 @@
     function logout() {
     sessionStorage.removeItem('userLogged');
     sessionStorage.removeItem('tipoUsuario');
-    window.location.href = "../index.html"; // Redireciona para a página de login
+    window.location.href = "../../index.html"; // Redireciona para a página de login
 }
 
-// Verifica se o usuário está logado
 if (sessionStorage.getItem('userLogged') === "true") {
     const tipoUsuario = sessionStorage.getItem('tipoUsuario');
+    const currentPath = window.location.pathname;
 
-    // Verifica o tipo de usuário e redireciona para o dashboard correspondente
-    if (tipoUsuario === "Administrador") {
-        window.location.href = "public/adm/admdashboard.html";
-    } else if (tipoUsuario === "Geral") {
-        window.location.href = "public/geral/geraldashboard.html";
-    } else if (tipoUsuario === "Manutenção") {
-        window.location.href = "public/manutencao/manutencaodashboard.html";
-    } else {
-        // Caso o tipo de usuário não seja reconhecido, redireciona para uma página de erro ou login
-        window.location.href = "../../index.html"; 
+    // Verifica o tipo de usuário e redireciona somente se estiver na página errada
+    if (tipoUsuario === "Administrador" && !currentPath.includes("admdashboard.html")) {
+        window.location.href = "../../public/adm/admdashboard.html";
+    } else if (tipoUsuario === "Geral" && !currentPath.includes("geraldashboard.html")) {
+        window.location.href = "../../public/geral/geraldashboard.html";
+    } else if (tipoUsuario === "Manutenção" && !currentPath.includes("manutencaodashboard.html")) {
+        window.location.href = "../../public/manutencao/manutencaodashboard.html";
+    } else if (!["Administrador", "Geral", "Manutenção"].includes(tipoUsuario)) {
+        window.location.href = "../../index.html";
     }
 } else {
     // Se o usuário não estiver logado, redireciona para a página de login
     window.location.href = "../../index.html";
 }
+
