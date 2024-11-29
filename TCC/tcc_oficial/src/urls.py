9fd87f2804cc6ea05ae762f16b40bd013bad89d4
EndrewBortoli <endrew.ryan977@gmail.com>
2024-11-29 14:07:37 -0300

    all pathd

--- TCC/tcc_oficial/src/urls.py
+++ TCC/tcc_oficial/src/urls.py
@@ -3,5 +3,22 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Rota inicial
+
+    # Rotas para a parte de Administração
     path('adm/admdashboard', views.adm_dashboard, name='adm_dashboard'),  # Rota para o painel de administração
+    path('adm/funcionarios/', views.funcionarios, name='funcionarios'),
+    path('adm/novo-feedback/', views.novo_feedback, name='novo_feedback'),
+    path('adm/novo-funcionario/', views.novo_funcionario, name='novo_funcionario'),
+    path('adm/novo-servico/', views.novoservico_adm, name='novo_servico_adm'),
+    path('adm/servico/', views.servico, name='servico'),
+    path('adm/feedback', views.feedback_adm, name='feedback_adm'),
+
+    # Rotas para o Dashboard Geral
+    path('geral/dashboard/', views.geral_dashboard, name='geral_dashboard'),
+    path('geral/feedback/', views.feedback_geral, name='feedback_geral'),
+    path('geral/novo-servico/', views.novoservico_geral, name='novo_servico_geral'),
+
+    # Rotas para a parte de Manutenção
+    path('manutencao/dashboard/', views.manutencao_dashboard, name='manutencao_dashboard'),
+    path('manutencao/detalhes/', views.detalhes, name='detalhes'),
 ]
--- TCC/tcc_oficial/src/views.py
+++ TCC/tcc_oficial/src/views.py
@@ -5,4 +5,37 @@ def index(request):
 
 def adm_dashboard(request):
     # Aqui você pode colocar a lógica necessária para o painel de administração, se houver
-    return render(request, 'adm/admdashboard.html')  # Certifique-se de que o arquivo existe
+    return render(request, 'adm/adm-dashboard.html')  # Certifique-se de que o arquivo existe
+
+def feedback_adm(request):
+    return render(request, 'adm/feedback.html')
+
+def funcionarios(request):
+    return render(request, 'adm/funcionarios.html')
+
+def novo_feedback(request):
+    return render(request, 'adm/novo-feedback.html')
+
+def novo_funcionario(request):
+    return render(request, 'adm/novo-funcionario.html')
+
+def novoservico_adm(request):
+    return render(request, 'adm/novo-servico.html')
+
+def servico(request):
+    return render(request, 'adm/servico.html')
+
+def geral_dashboard(request):
+    return render(request, 'geral/geral-dashboard.html')
+
+def feedback_geral(request):
+    return render(request, 'geral/feedback.html')
+
+def novoservico_geral(request):
+    return render(request, 'geral/novo-servico.html')
+
+def manutencao_dashboard(request):
+    return render(request, 'manutencao/manutencao-dashboard.html')
+
+def detalhes(request):
+    return render(request, 'manutencao/detalhes.html')
